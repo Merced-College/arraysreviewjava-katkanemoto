df5103b719aaf642c09b6fb100c9af26ebccd893
Kathleen Kanemoto <5721632+katkanemoto@users.noreply.github.com>
2026-01-22 18:12:50 +0000

    comment

--- BlackJack.java
+++ BlackJack.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class BlackJack {
 
+    //this is a constant - I cannot change these arrays
     private static final String[] SUITS = { "Hearts", "Diamonds", "Clubs", "Spades" };
     private static final String[] RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",
             "Ace" };
